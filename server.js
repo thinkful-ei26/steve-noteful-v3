b6aa900a5922dd06a39ab9840c724fcecfc7f050
Stephen Anderson <anderssj@usc.edu>
2018-11-27 14:03:02 -0700

    queries

--- server.js
+++ server.js
@@ -1,54 +1,70 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const morgan = require('morgan');
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
 
-const { PORT } = require('./config');
+const {PORT, MONGODB_URI} = require('./config')
 
-const notesRouter = require('./routes/notes');
+const notesRouter = require('./routes/notes')
 
 // Create an Express application
-const app = express();
+const app = express()
 
 // Log all requests. Skip logging during
-app.use(morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'common', {
-  skip: () => process.env.NODE_ENV === 'test'
-}));
+app.use(
+  morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'common', {
+    skip: () => process.env.NODE_ENV === 'test'
+  })
+)
 
 // Create a static webserver
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // Parse request body
-app.use(express.json());
+app.use(express.json())
 
 // Mount routers
-app.use('/api/notes', notesRouter);
+app.use('/api/notes', notesRouter)
 
 // Custom 404 Not Found route handler
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // Custom Error Handler
 app.use((err, req, res, next) => {
   if (err.status) {
-    const errBody = Object.assign({}, err, { message: err.message });
-    res.status(err.status).json(errBody);
+    const errBody = Object.assign({}, err, {message: err.message})
+    res.status(err.status).json(errBody)
   } else {
-    console.error(err);
-    res.status(500).json({ message: 'Internal Server Error' });
+    console.error(err)
+    res.status(500).json({message: 'Internal Server Error'})
   }
-});
+})
 
 // Listen for incoming connections
 if (process.env.NODE_ENV !== 'test') {
-  app.listen(PORT, function () {
-    console.info(`Server listening on ${this.address().port}`);
-  }).on('error', err => {
-    console.error(err);
-  });
+  mongoose
+    .connect(
+      MONGODB_URI,
+      {useNewUrlParser: true}
+    )
+    .catch(err => {
+      console.error(`ERROR: ${err.message}`)
+      console.error('\n === Did you remember to start `mongod`? === \n')
+      console.error(err)
+    })
+
+  app
+    .listen(PORT, function() {
+      console.info(`Server listening on ${this.address().port}`)
+    })
+    .on('error', err => {
+      console.error(err)
+    })
 }
 
-module.exports = app; // Export for testing
+module.exports = app // Export for testing
